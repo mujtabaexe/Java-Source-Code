1b7a7d7e40589589291f46373a443477c7cca673
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-08-05 12:12:05 +0500

    Types of variables

---
+++ variables.java
@@ -0,0 +1,45 @@
+public class Variables{}
+
+        public static void main(String[] args){
+        System.out.println("Types of Variables are; ");
+
+        byte age= 34;
+        int num=54;
+        char ch='A';
+        float f1= 5.6f;
+        double d1= 2.44;
+        short sage=93;
+        long ageDino= 302884876l;
+        boolean a = true;
+        String s="Sahibzada Ahmad Mujtaba";
+
+        System.out.print("Byte: ");
+        System.out.println(age);
+        
+        System.out.print("Int: ");
+        System.out.println(num);
+
+        System.out.print("Char: ");
+        System.out.println(ch);
+        
+        System.out.print("Float: ");
+        System.out.println(f1);
+
+        System.out.print("Double: ");
+        System.out.println(d1);
+
+        System.out.print("Short: ");
+        System.out.println(sage);
+
+        System.out.print("Long: ");
+        System.out.println(ageDino);
+
+        System.out.print("Boolean: ");
+        System.out.println(a);
+
+        System.out.print("String: ");
+        System.out.println(s);
+
+
+    }
+ 
