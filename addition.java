739ca14c15675e2cc6a323d71927c15f602de275
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-08-05 12:11:33 +0500

    Adding Numbers

---
+++ addition.java
@@ -0,0 +1,10 @@
+public class addition {}
+
+public static void main(String[] args){
+    int num1= 6;
+    int num2= 3;
+    int num3= 1;
+    int sum= num1 + num2 + num3;
+    System.out.print("The sum of the numbers is: ");
+    System.out.print(sum);
+}
