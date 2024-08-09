afe402bca605e902d4b739c2307605f724500423
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-08-09 15:54:54 +0500

    Checking Inputs

---
+++ rollNo.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner;
+
+public class rollNo{
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Roll No: ");
+        int roll = input.nextInt();
+        System.out.print("Your Roll No is: " + roll);
+
+    }
+}
