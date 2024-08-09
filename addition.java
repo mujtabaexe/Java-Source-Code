65674c1ef7b05f7cc070f92ce86ca38ae1112c8a
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-08-09 22:25:02 +0500

    Addition of numbers

--- addition.java
+++ addition.java
@@ -1,10 +1,11 @@
-public class addition {}
+public class Addition {
 
-public static void main(String[] args){
-    int num1= 6;
-    int num2= 3;
-    int num3= 1;
-    int sum= num1 + num2 + num3;
-    System.out.print("The sum of the numbers is: ");
-    System.out.print(sum);
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 3;
+        int num3 = 1;
+        int sum = num1 + num2 + num3;
+        System.out.print("The sum of the numbers is: ");
+        System.out.print(sum);
+    }
 }
