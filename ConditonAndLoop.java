c2572072b7519261a1b6630a97af5b15d81fe0c0
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-08-10 22:29:09 +0500

    Coniditions and loops

--- ConditonAndLoop.java
+++ ConditonAndLoop.java
@@ -24,6 +24,11 @@ public class ConditonAndLoop {
         System.out.println(temp);
     }
 
+    // In a do-while loop the body will must be executed once and then it will check the condition
+    int num = 1;
+    do{
+        System.out.println("Hello world");
+    } while(num!=1);
 
 
     }
