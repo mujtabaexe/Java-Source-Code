b72d2fb8e05ece7e26cdda82ec27ce2a42577e17
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-08-10 20:54:24 +0500

    Printing Natural and even numbers using a loop

---
+++ NaturalNumbers.java
@@ -0,0 +1,18 @@
+public class NaturalNumbers {
+    public static void main(String[] args){
+
+        // Loop for Natural numbers
+        for(int n=1;n<=100 ; n++){
+            System.out.println(n);
+
+        }        
+
+        //Loop for first 100 Even numbers
+        for (int even = 0; even<=100; even+=2) {
+            System.out.println(even);
+            
+        }
+
+
+    }
+}
