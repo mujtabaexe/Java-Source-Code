71f2759c7d637138b6faabf7e57ab4d0888b5c2c
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-08-10 19:06:45 +0500

    Codition and loops are performed

---
+++ ConditonAndLoop.java
@@ -0,0 +1,31 @@
+public class ConditonAndLoop {
+
+    public static void main(String[] args){
+    // Basic if condition    
+    if(true){
+    System.out.println("Hello World");
+
+    }
+    int a = 10;
+
+    if(a==10){
+        System.out.println("Yes a = "+ a);
+
+    }
+    // While loop
+    int count = 1;
+    while (count !=6){
+    System.out.println(count);
+    count++;
+        }
+    //For Loop
+
+    for (int temp=1; temp!=11; temp++){
+        System.out.println(temp);
+    }
+
+
+
+    }
+    
+}
