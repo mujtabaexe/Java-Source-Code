1afa2ce560d98cee2d06ae5b20bd1551d6696dfd
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-08-12 20:38:26 +0500

    Case checking

---
+++ CaseCheck.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any character to check its case: ");
+        char ch = sc.next().trim().charAt(0);
+
+        if(ch>='a' && ch<='z'){
+            System.out.println("Lowercase");
+        }else{
+            System.out.println("Uppercase");
+        }
+
+    }
+    
+}
