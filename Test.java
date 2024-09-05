7ba66f3a0aafc5342b42b43c4f16eea87ff704b2
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-09-05 16:55:26 +0500

    Testing arguments method

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    public static void main(String[] mujtaba) {
+        for (int i = 0; i < mujtaba.length; i++) {
+            System.out.println(mujtaba[i]);
+        }
+    }    
+}
+
+// To give arguments you have to compile it first using javac and then to give argumengts... do this---> 
+// java Test Ahmad Mujtaba (and so on... The names here are arguments)
