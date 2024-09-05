42d4f745c960811ed30f59b1d10d1deb1a6995f6
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-09-05 16:48:57 +0500

    Addition

--- addition.java
+++ addition.java
@@ -1,5 +1,6 @@
-public class Addition {}
+public class Addition {
 
+    
     public static void main(String[] args) {
         int num1 = 6;
         int num2 = 3;
@@ -7,4 +8,5 @@ public class Addition {}
         int sum = num1 + num2 + num3;
         System.out.print("The sum of the numbers is: ");
         System.out.print(sum);
+    }
 }
