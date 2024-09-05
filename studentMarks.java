15b16315c98a57becbd53683d311066ad4bd1f8b
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-09-05 16:49:33 +0500

    Calculating percentage

--- studentMarks.java
+++ studentMarks.java
@@ -1,29 +1,28 @@
 import java.util.Scanner;
 
-private class Object{}
 public class StudentMarks {
     
-}
-
-public static void main(String[] argss){
-
-    Scanner sc = new Scanner(System.in);
-
-    System.out.print("Enter your marks in Physics: ");
-    int num1= sc.nextInt();
-    System.out.print("Enter your marks in Biology: ");
-    int num2= sc.nextInt();
-    System.out.print("Enter your marks in English: ");
-    int num3= sc.nextInt();
-    System.out.print("Enter your marks in Maths: ");
-    int num4= sc.nextInt();
-    System.out.print("Enter your marks in Chemistry: ");
-    int num5= sc.nextInt();
-
-    int per= (num1 + num2 + num3 + num4 + num5)*100/500;
-
-    System.out.println("Your percentage is: ");
-    System.out.print(per);
-
-
+    
+    public static void main(String[] argss){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your marks in Physics: ");
+        int num1= sc.nextInt();
+        System.out.print("Enter your marks in Biology: ");
+        int num2= sc.nextInt();
+        System.out.print("Enter your marks in English: ");
+        int num3= sc.nextInt();
+        System.out.print("Enter your marks in Maths: ");
+        int num4= sc.nextInt();
+        System.out.print("Enter your marks in Chemistry: ");
+        int num5= sc.nextInt();
+        
+        int per= (num1 + num2 + num3 + num4 + num5)*100/500;
+        
+        System.out.println("Your percentage is: ");
+        System.out.print(per);
+        
+        
+    }
 }
