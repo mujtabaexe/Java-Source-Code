ed6527714b8f4a1bd96796071577c5d1729a33b9
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-09-05 16:57:48 +0500

    Type casting

--- TypeCasting.java
+++ TypeCasting.java
@@ -2,24 +2,23 @@
 import java.util.Scanner;
 public class TypeCasting {
     
-}
-
-public static void main(String[] args){
-
+    
+    public static void main(String[] args){
+        
     Scanner input = new Scanner(System.in);
     System.out.print("Enter a number: "); // If you put integer its type casting will occur and will be considered as Floating number
     // Left side should be greater than the right side
     float num = input.nextFloat(); // Should use a compatible data type
     System.out.println("The number you entered is: " + num); 
-
-
+    
+    
     // TYPE CASTING
-
+    
     int i1 = (int)(num);
     int i2 = (int)(66.987);
     System.out.println("The only int part is printed of your selected number as: "+i1);
     System.out.println("The integer part of the stored number is: "+i2);
-
+    
     int a = 257;
     //byte b = a; // it will give an error because a is of type integer and you are converting it to a smaller type
     byte b = (byte)(a); // 257%256=1 is happening here
@@ -27,6 +26,7 @@ public static void main(String[] args){
     
     int number = 'A';
     System.out.println(number);
-
-
+    
+    
+}
 }
