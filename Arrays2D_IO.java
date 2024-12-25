cda079009cbd4d765b1726215124b1a2264e56b8
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2024-12-26 01:16:19 +0500

    practice

--- Arrays2D_IO.java
+++ Arrays2D_IO.java
@@ -2,7 +2,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Arrays2DInput {
+public class Arrays2D_IO {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows for 2D array: ");
@@ -10,16 +10,31 @@ public class Arrays2DInput {
         System.out.print("Enter the number of columns for 2D array: ");
         int num2 = sc.nextInt();
 
+
         int[][] arr = new int[num1][num2];
         for(int row = 0; row<num1 ;row++) {
             for(int col = 0 ; col<num2 ; col++){
-                System.out.printf("Enter element for row %d and  column %d: ", row, col);
+                System.out.printf("Enter element for row %d and  column %d for Array 1: ", row, col);
                 arr[row][col] = sc.nextInt();
             }
         }
         printArr(arr);
         enhancedForLoop(arr);
         printRowAndCol(arr);
+
+
+
+        int[][] arr2 = new int[2][2];
+        for(int row = 0 ; row <arr2.length;row++){
+            for (int col = 0 ; col<arr2[row].length;col++){
+                System.out.printf("Enter element for row %d and  column %d for Array 2: ", row, col);
+                arr2[row][col]= sc.nextInt();
+            }
+        }
+
+        for(int[] a : arr2){
+            System.out.println(Arrays.toString(a));
+        }
         
         sc.close();
     }
@@ -34,13 +49,17 @@ public class Arrays2DInput {
     
     public static void enhancedForLoop(int[][] arr){
         for(int[] a : arr){
-            System.out.println(Arrays.toString(a));
+            System.out.print(Arrays.toString(a));
+            for(int b : a){
+                System.out.print(b);
+            }
+            System.out.println();
         }
         
     }
     public static void printRowAndCol(int[][] arr){
         for(int row = 0 ; row<arr.length; row++){
-            for(int col = 0 ; col<arr.length; col++){
+            for(int col = 0 ; col<arr[row].length; col++){
                 System.out.print(arr[row][col]+" ");
             }
             System.out.println();
---
+++ ReversedArray.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+
+
+
+public class ReversedArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int[] temp = new int[5];
+        for(int i=arr.length-1;i>=0;i--){
+            temp[4-i]=arr[i];
+        }
+        System.out.println(Arrays.toString(temp));
+    }
+}
