dad2cc0ebf5d09e6854f4220aa59c08eccabb39b
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2025-01-06 17:11:54 +0500

    Practice

--- ReversingArray1.java
+++ ReversingArray1.java
@@ -8,7 +8,7 @@ public class ReversingArray1 {
         int[] arr = {1,2,3,4,5};
         int[] temp = new int[5];
         for(int i=arr.length-1;i>=0;i--){
-            temp[4-i]=arr[i];
+            temp[arr.length-1-i]=arr[i];
         }
         System.out.println(Arrays.toString(temp));
     }
